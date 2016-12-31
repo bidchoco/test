48e54ff8c1b2825f3568a2cda0d9b401382f9627
Ramhari Gyawali <bid.choco@gmail.com>
2016-12-31 22:38:40 +0545

    oct 2016 solution added

--- src/javaconsoleapplication/JavaConsoleApplication.java
+++ src/javaconsoleapplication/JavaConsoleApplication.java
@@ -17,9 +17,80 @@ public class JavaConsoleApplication {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        int[] a = {3, 3, 2, 6, 7};
-          System.out.println(isTwinoid(a));
-          //System.out.println(countDigit(1320003334, 3));
+        int[] a = {4, 2, -3, 12};
+          //System.out.println(isSuper(a));
+          //System.out.println(isEvenSubset(8, 12));
+          
+    }
+    //October 29th, 2016 Qno 1//
+    public static int[] fill(int[] a, int k, int n){
+        int[] arr = new int[n];
+        if(k<1 || n < 1 || n < k || a.length < k)
+            return null;
+        for(int i = 0; i<k; i++){
+            arr[i] = a[i];
+        }
+        for(int i = k; i< n; i++){
+            arr[i] = a[i%k];
+        }
+        return arr;
+    }
+    //October 29th, 2016 Qno 2//
+    public static int isSuper(int[] a){
+        int isSuper = 1;
+        int sum = 0;
+        for(int i = 0; i < a.length; i++){
+            if(a[i] > sum)
+                sum += a[i];
+            else{
+                isSuper = 0;
+                break;
+            }                
+        }
+        return isSuper;
+    }
+    
+    //October 29th, 2016 Qno 3//
+    public static int isHollow(int[] a){
+        int isHollow = 0;
+        int zeroCount = 0;
+        int leftCounter = 0;
+        int rightCounter = 0;
+        boolean isLeft = true;
+        if(a[0] != 0 && a[a.length -1] != 0)
+        {
+            for(int i = 0; i < a.length; i++){
+                if(a[i] != 0  && isLeft){
+                    leftCounter++;
+                }
+                else if(a[i] != 0  && !isLeft)
+                {
+                    rightCounter++;
+                }
+                else
+                {
+                    zeroCount++;
+                    isLeft = false;                            
+                }
+            }
+            if(zeroCount > 2 && leftCounter == rightCounter)
+                isHollow = 1;
+        }
+        return isHollow;
+    }
+    
+    //Nov 26 2016 Qno 1//
+    public static int isEvenSubset(int m, int n){
+        int isEvenSubset = 0;
+        for(int i = 2; i < m; i++){
+            if(i%2 == 0 && m%i == 0 && n%i == 0){                
+                isEvenSubset = 1;
+                continue;
+            }
+            else if(i%2 == 0 && m%i == 0 && n%i !=0)
+                isEvenSubset = 0;
+        }
+        return isEvenSubset;
     }
     
     //Nov 26 2016 Qno 2//
@@ -27,10 +98,6 @@ public class JavaConsoleApplication {
         int isTwinoid = 1;
         for(int i = 0; i<a.length; i++)
         {
-//            if(isTwinoid != 1 && i != a.length -1 && a[i]%2==0 && a[i+1]%2==0){
-//                isTwinoid = 1;
-//                continue;
-//            }
             if( i> 0 &&isTwinoid == 1 && i != a.length -1 && a[i-1] %2 == 0 && a[i]%2==0 && a[i+1]%2==0){
                 isTwinoid = 0;
                 break;
