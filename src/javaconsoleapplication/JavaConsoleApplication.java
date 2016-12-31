2de4c2ccb113f668c9d1f2ef459d81db05697b28
om raut <om raut>
2016-12-31 14:51:33 +0545

    Second commit some addition

--- src/javaconsoleapplication/JavaConsoleApplication.java
+++ src/javaconsoleapplication/JavaConsoleApplication.java
@@ -16,8 +16,41 @@ public class JavaConsoleApplication {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int[] a = {4, 9, 6, 7, 3};
-          System.out.println(isBunkerArray(a));
+        int[] a = {-5, 2, -2};
+          System.out.println(isBalanced(a));
+          //System.out.println(countDigit(1320003334, 3));
+    }
+    //Nov 26 2016 Qno 3//
+    
+    public static int isBalanced(int[] a){
+        int isBalanced = 0;
+        boolean finalBreak = false;
+        for(int i = 0; i<a.length; i++){
+            if(finalBreak)
+                break;
+            for(int j = 0; j<a.length; j++){
+                if(a[i] == -a[j]){
+                    isBalanced = 1;
+                    continue;
+                }
+                if(j==a.length -1 && isBalanced == 0)
+                    finalBreak = true;                
+            }
+        }
+        return isBalanced;
+    }
+    
+    //Dec 10 2016 Qno. 1//
+    
+    public static int countDigit(int a, int num){
+        int count = 0;
+        while(a%10!=0 || a/10 > 0) {
+            if(a%10 == num)
+            count++;
+            a=a/10; 
+            }
+               
+        return count;
     }
     //Dec 10 2016 Qno. 2//
     public static int isBunkerArray(int[] a){
